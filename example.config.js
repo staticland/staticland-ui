c486e9212a89d8472569c0e4a5aec3ed7bd02111
sethvincent <sethvincent@gmail.com>
2017-01-20 12:39:36 -0800

    update example config

--- example.config.js
+++ example.config.js
@@ -1,4 +1,13 @@
-module.exports = {
-  server: 'http://127.0.0.1:3322',
-  clientHost: 'http://127.0.0.1:9966'
+var config = {
+  development: {
+    server: 'http://127.0.0.1:3322',
+    clientHost: 'http://127.0.0.1:9966'
+  },
+  production: {
+    server: 'https://api.static.land',
+    clientHost: 'https://app.static.land'
+  }
 }
+
+var env = process.env.NODE_ENV || 'development'
+module.exports = config[env]
