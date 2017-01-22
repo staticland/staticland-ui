79c8405d3f15131a47a5814b08298aa83d5916c9
sethvincent <sethvincent@gmail.com>
2017-01-21 18:55:26 -0800

    redirect site page if not logged in

--- pages/site.js
+++ pages/site.js
@@ -8,11 +8,12 @@ var register = require('../elements/register-form')
 
 module.exports = function (state, prev, send) {
   var authenticated = state.account.authenticated
+  var checkingAuth = state.account.checkingAuth
   var domain = state.location.search.domain
   var fetchedSite = state.sites.fetchedSite
   var fetching = state.sites.fetchingSite
   var site = state.sites.currentSite
-  var ready = authenticated && fetchedSite && !fetching
+  var ready = authenticated && !checkingAuth
 
 
 
@@ -43,29 +44,35 @@ module.exports = function (state, prev, send) {
     <p class="mt4"><a class="link pa2 bg-red hover-bg-dark-red white mb4 pointer" onclick=${onclickDestroySite}>Yes I really want to delete ${domain}!</a></p>
   </div>`
 
-  if (!fetching && !fetchedSite) {
-    send('sites:site', domain)
-    return html`<div></div>`
-  } else if (fetching) {
-    return html`<div class="">
-      <h2 class="f5 ttu dark-gray">${domain}</h2>
-      <p>loading</p>
-    </div>`
+  if (ready) {
+    if (!fetching && !fetchedSite) {
+      send('sites:site', domain)
+      return html`<div></div>`
+    } else if (fetching) {
+      return html`<div class="">
+        <h2 class="f5 ttu dark-gray">${domain}</h2>
+        <p>loading</p>
+      </div>`
+    } else {
+      return html`<div class="">
+        <h2 class="f2 ttu dark-gray mb4">${domain}</h2>
+        <p><a href="https://${domain}" class="link pa2 bg-blue hover-bg-dark-blue white mb4">View ${domain}</a></p>
+
+        <h3 class="f3 dark-gray mt5">Site information</h3>
+        <p><b>Created:</b> ${site.created}</p>
+        ${site.updated ? html`<p><b>Last updated:</b> ${site.updated}</p>` : ''}
+        <p><b>Deploys:</b> ${site.deploys}</p>
+
+        <h3 class="f3 dark-gray mt5">Delete this site</h3>
+        ${state.sites.confirmDestroyingSite === true 
+          ? destroyForReal
+          : destroyAreYouSure
+        }
+      </div>`
+    }
   } else {
-    return html`<div class="">
-      <h2 class="f2 ttu dark-gray mb4">${domain}</h2>
-      <p><a href="https://${domain}" class="link pa2 bg-blue hover-bg-dark-blue white mb4">View ${domain}</a></p>
-
-      <h3 class="f3 dark-gray mt5">Site information</h3>
-      <p><b>Created:</b> ${site.created}</p>
-      ${site.updated ? html`<p><b>Last updated:</b> ${site.updated}</p>` : ''}
-      <p><b>Deploys:</b> ${site.deploys}</p>
-
-      <h3 class="f3 dark-gray mt5">Delete this site</h3>
-      ${state.sites.confirmDestroyingSite === true 
-        ? destroyForReal
-        : destroyAreYouSure
-      }
-    </div>`
-  } 
+    send('go', '/')
+    return html`<div></div>`
+  }
+  
 }
